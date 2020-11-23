f45b8355ff98d658bfa7ed66d3654622e9cd7fda
daniel <daniel@hotmail.es>
2020-11-23 20:54:43 +0000

    diseño responsive

---
+++ scripts.js
@@ -0,0 +1,33 @@
+$('.menu-hidden').on('click', function () {
+  $('.article').toggle();
+  $('.my-nav').toggle();
+});
+
+if ($(window).width() >= 768 && $(window).width() <= 1024) {
+  $('ul').removeClass('flex-column');
+  $('li .d-none').removeClass('d-none');
+}
+
+if ($(window).width() <= 1024) {
+  $('li .d-none').removeClass('d-none');
+}
+
+if ($(window).width() <= 360) {
+  $('.col-4 .responsive').removeClass('col-4');
+  $('.responsive').addClass('col-6');
+
+  $('.col-2 .responsive').removeClass('col-2');
+  $('.col-2 .responsive').addClass('col-3');
+}
+
+if ($(window).width() >= 640 && $(window).width() <= 768) {
+  $('.responsive').removeClass('col-2');
+  $('.responsive').addClass('col-3');
+}
+
+if ($(window).width() >= 768 && $(window).width() <= 1024) {
+  $('.responsive').removeClass('col-2');
+  $('.responsive').addClass('col-3');
+}
+
+
